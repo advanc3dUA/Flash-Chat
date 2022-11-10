8bebfbae19412ce35bd321441a7606d99575a46a
advanc3dUA <fash1k@gmail.com>
2022-11-10 11:22:08 +0100

    added: separated own & other messages with different bubble style

--- Flash Chat iOS13/Views/MessageCell.swift
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -3,6 +3,7 @@ import UIKit
 
 class MessageCell: UITableViewCell {
 
+    @IBOutlet weak var leftImageView: UIImageView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var rightImageView: UIImageView!
     @IBOutlet weak var messageBubble: UIView!
